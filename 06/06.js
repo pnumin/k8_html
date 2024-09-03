545c71cfca26d4c2644f5bba35c422ec94e466d8
pnumin <pnumin@pusan.ac.kr>
2024-09-03 16:00:44 +0900

    자바스트립트 기초

---
+++ 06/06.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  //이미지 가져오기
+  const imgs = document.querySelectorAll('.dice > img') ;
+  //버튼 가져오기
+  const bts = document.querySelectorAll('button') ;
+  //결과 가져오기 
+  const msg = document.querySelector('#msg') ;
+
+  console.log(bts);
+
+  //버튼 클릭
+  bts[0].addEventListener('click', ()=>{
+    
+  });
+});
