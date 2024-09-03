e5df55a40cf0e9cd11893b4a69298e9ea9cca627
pnumin <pnumin@pusan.ac.kr>
2024-09-03 17:17:35 +0900

    주사위게임

--- 06/06.js
+++ 06/06.js
@@ -6,10 +6,31 @@ document.addEventListener('DOMContentLoaded', ()=>{
   //결과 가져오기 
   const msg = document.querySelector('#msg') ;
 
-  console.log(bts);
+  // console.log(bts);
 
   //버튼 클릭
-  bts[0].addEventListener('click', ()=>{
-    
-  });
+  for (let bt of bts) {
+    // console.log(bt);
+    bt.addEventListener('click', ()=>{
+      //컴퓨터 랜덤수 
+      let comN = Math.floor(Math.random()*6) + 1 ; //1~6
+      imgs[0].setAttribute('src', `../img/${comN}.png`);
+      imgs[0].setAttribute('alt', `${comN}.png`);
+
+      //사용자 선택수 
+      console.log(bt.textContent.charAt(0));
+      let userN = parseInt(bt.textContent.charAt(0)) ;
+      imgs[1].setAttribute('src', `../img/${userN}.png`);
+      imgs[1].setAttribute('alt', `${userN}.png`);
+
+      //결과출력
+      if (comN === userN) {
+        msg.textContent = '맞음';
+      }
+      else {
+        msg.textContent = '틀림';
+      }
+    });
+  }
+
 });
