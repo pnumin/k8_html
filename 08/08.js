9291b355aaeb8b7d8064a096c0e88fe76fcf5e14
pnumin <pnumin@pusan.ac.kr>
2024-09-05 15:41:02 +0900

    업다운

--- 08/08.js
+++ 08/08.js
@@ -13,15 +13,27 @@ document.addEventListener('DOMContentLoaded', ()=>{
   bt1.addEventListener('click', (e)=>{
     e.preventDefault();
 
-    if (flag == false) {
+    //if (flag == false) {
+    if (!flag) {
       n = Math.floor(Math.random() * 100) + 1 ; //1 ~ 100
       console.log('n=', n) ;
-
       flag = true;
+
+      if (txt1.style.display == 'none') {
+        img.setAttribute('src', '../img/what.png') ;
+        txt1.style.display = 'inline' ;
+        txt1.value = '' ;
+        txt1.focus() ;
+        bt1.innerHTML = '확인';
+        return ;
+      }
     }
     
     if (n === parseInt(txt1.value)) {
       img.setAttribute('src', '../img/good.png') ;
+      txt1.style.display = 'none' ;
+      bt1.innerHTML = '번호를 다시 생성하세요.';
+      flag = false ;
     }
     else if ( n > parseInt(txt1.value)) {
       img.setAttribute('src', '../img/up.png') ;
