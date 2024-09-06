22ce9a5c14781e3819da8b3537635807ada62ad8
pnumin <pnumin@pusan.ac.kr>
2024-09-06 17:51:01 +0900

    로또번호생성

--- 10/10.js
+++ 10/10.js
@@ -1,3 +1,9 @@
+const disp = ( divNum, divplus,divBonus, dsp ) => {
+  divNum.style.display = dsp;
+  divplus.style.display = dsp;
+  divBonus.style.display = dsp;
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
   //요소 가져오기
   const divNum = document.querySelector('#divNum') ;
@@ -6,9 +12,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const bt = document.querySelector('.sec > button') ;
 
   //요소 숨기기
-  divNum.style.display = 'none';
-  divplus.style.display = 'none';
-  divBonus.style.display = 'none';
+  // divNum.style.display = 'none';
+  // divplus.style.display = 'none';
+  // divBonus.style.display = 'none';
+  disp(divNum, divplus,divBonus, 'none');
 
   //버튼 클릭
   bt.addEventListener('click', (e)=>{
@@ -20,9 +27,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
       if ( !arr.includes(n) ) arr.push(n) ;
     }
 
+    let arrBonus = arr.splice(6,1) ;
+    arr.sort((a,b) => a - b);
+    console.log(arrBonus);
+    
+    arr = arr.map( item => 
+                    `<span class='sp${Math.floor(item/10)}'>${item}</span>`) ;
+    arr = arr.join('');
+    divNum.innerHTML = arr ;
     console.log(arr);
 
+    arrBonus = arrBonus.map(item => 
+                            `<span class='sp${Math.floor(item/10)}'>${item}</span>`);
+    arrBonus = arrBonus.join('');
+    divBonus.innerHTML = arrBonus ;
 
+    //요소 숨기기
+    // divNum.style.display = 'block';
+    // divplus.style.display = 'block';
+    // divBonus.style.display = 'block';
+    disp(divNum, divplus,divBonus, 'block');
   });
 
 });
