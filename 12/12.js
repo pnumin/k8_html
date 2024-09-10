35d90e24510f073f89dcacd6c23c00730e4c7683
pnumin <pnumin@pusan.ac.kr>
2024-09-10 12:52:16 +0900

    박스오피스

--- 12/12.js
+++ 12/12.js
@@ -1,6 +1,30 @@
 //OPEN API 데이터 가져오기
 const getData = (selDt, ul) => {
   console.log(selDt);
+  const testAPI = '82ca741a2844c5c180a208137bb92bd7' ;
+  let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?` ;
+  url = `${url}key=${testAPI}&targetDt=${selDt}`;
+
+  console.log(url);
+
+  fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+      let dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList ;
+      console.log(dailyBoxOfficeList)
+
+      let tm = dailyBoxOfficeList.map(item => 
+        `<li class='mvli'>
+            <span class='rank'>${item.rank}</span>
+            <sapn class='movieNm'>${item.movieNm}</sapn>
+          </li>`)
+
+      tm = tm.join('')
+      ul.innerHTML = tm ;
+      console.log(tm)
+    })
+    .catch(err => console.error(err)) ;
+
 }
 
 
