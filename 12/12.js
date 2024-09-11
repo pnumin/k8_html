b657ce2097d5b8d590c9b1d76e3bf0431876ccb1
pnumin <pnumin@pusan.ac.kr>
2024-09-11 12:45:53 +0900

    박스오피스 라디오

--- 12/12.js
+++ 12/12.js
@@ -59,12 +59,39 @@ const getYesterday = () => {
   return `${year}-${month}-${day}` ;
 }
 
+//radio 값 가져오기
+const getGubun = () => {
+  //radio 요소 가져오기
+  // const r1 = document.querySelector('#r1') ;
+  // const r2 = document.querySelector('#r2') ;
+  // const r3 = document.querySelector('#r3') ;
+
+  // console.log("r1 =", r1.checked) ;
+  // console.log("r2 =", r2.checked) ;
+  // console.log("r3 =", r3.checked) ;
+
+  // if (r1.checked) return r1.value ;
+  // else if (r2.checked) return r2.value ;
+  // else if (r3.checked) return r3.value ;
+
+  
+  // radio버튼의 클릭된 것만 가져오기
+  const gubun = document.querySelector('input[name=mvGubun]:checked') ;  
+  console.log('gubun = ', gubun.value) ;
+  return gubun.value ;
+
+}
+
+
 //DOM 생성후
 document.addEventListener('DOMContentLoaded', ()=>{
   //요소 가져오기
   const dt = document.querySelector('#dt') ;
-  const ul = document.querySelector('.sec > ul') ;
-  const sel1 = document.querySelector('#sel1') ;
+  const ul = document.querySelector('.sec > ul') ; 
+  // const fieldset = document.querySelector('fieldset') ;
+  // const radios = document.querySelectorAll('input[type=radio]') ; 
+  // const radios = document.getElementsByName('mvGubun');
+  const radios = document.querySelectorAll('input[name=mvGubun]') ;
 
   //어제 날짜 구하기
   let yesterday = getYesterday() ;
@@ -76,16 +103,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
   //date 기본값
   dt.value = yesterday ;
 
+  //gubun 값
+  console.log(getGubun());
+
   //기본 첫 페이지 보이기
-  getData(dt.value.replaceAll('-',''), ul, sel1.value);
+  getData(dt.value.replaceAll('-',''), ul, getGubun());
 
   //데이터 가져오기
   dt.addEventListener('change', ()=>{
-    getData(dt.value.replaceAll('-',''), ul, sel1.value);
+    getData(dt.value.replaceAll('-',''), ul, getGubun());
   }) ;
 
-  sel1.addEventListener('change', ()=>{
-    getData(dt.value.replaceAll('-',''), ul, sel1.value);
-  }) ;
-
+  // fieldset.addEventListener('click', ()=> {
+  //   getData(dt.value.replaceAll('-',''), ul, getGubun());
+  // });
+  
+  for(let radio of radios) {
+    radio.addEventListener('click' , ()=>{
+      if (radio.checked) getData(dt.value.replaceAll('-',''), ul, radio.value);
+    })
+  }
 });
